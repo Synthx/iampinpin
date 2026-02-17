de244158b4d322fc0b3e1617e2c35efdf5decbdb
Taniel Rémi <taniel.remi@gmail.com>
2026-02-17 08:35:07 +0100

    💄 mobile first layout

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import astro from "eslint-plugin-astro";
+import js from "@eslint/js";
+import tsEslint from "eslint-plugin-astro";
+
+export default defineConfig([
+  globalIgnores([".astro", "dist"]),
+  js.configs.recommended,
+  tsEslint.configs.recommended,
+  astro.configs.recommended,
+  astro.configs["jsx-a11y-strict"],
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astro.parser,
+      parserOptions: {
+        parser: tsEslint.parser,
+        extraFileExtensions: [".astro"],
+        sourceType: "module",
+        ecmaVersion: "latest",
+        project: "./tsconfig.json",
+      },
+    },
+  },
+]);

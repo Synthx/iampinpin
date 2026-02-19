2cfc6176205f80c03027f04d8efa879dbf5c3f7a
Taniel Rémi <taniel.remi@gmail.com>
2026-02-19 08:31:16 +0100

    :recycle: rework card management

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,6 +2,7 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 import { digitalGardenLoader } from "@/loaders/garden-loader.ts";
+import { NodeType } from "@/types/digital-garden.ts";
 
 const changelog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/contents/changelog" }),
@@ -16,7 +17,7 @@ const garden = defineCollection({
   loader: digitalGardenLoader({ base: "./src/contents/garden" }),
   schema: z.object({
     title: z.string(),
-    type: z.enum(["project", "talk", "hobby"]),
+    type: NodeType,
     date: z.coerce.date(),
     image: z.string().optional(),
     url: z.string().optional(),
